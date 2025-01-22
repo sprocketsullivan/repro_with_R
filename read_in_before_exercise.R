e013c1467fb59b3fd01b66744af9cc8c854bc610
Ulfgit config --global user.email toelch@gmail.com <you@example.com>
2025-01-22 14:36:21 +0100

    uncommeted read_in_before_exercise

--- read_in_before_exercise.R
+++ read_in_before_exercise.R
@@ -1,14 +1,14 @@
 # # load libraries
 # # check.packages function: install and load multiple R packages.
 # # Check to see if packages are installed. Install them if they are not, then load them into the R session.
-# check.packages <- function(pkg) {
-#   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-#   if (length(new.pkg)) {
-#     install.packages(new.pkg, dependencies = TRUE)
-#   }
-#   sapply(pkg, require, character.only = TRUE)
-# }
-#
+check.packages <- function(pkg) {
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) {
+    install.packages(new.pkg, dependencies = TRUE)
+  }
+  sapply(pkg, require, character.only = TRUE)
+}
+
 # # Usage example
 packages <- c("tidyverse", "arm", "bbmle")
 check.packages(packages)
